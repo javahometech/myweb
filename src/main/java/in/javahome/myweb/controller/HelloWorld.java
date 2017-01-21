aec76d518572b48b513c74c364e073646cec22ae
Hari <hari@javahome.in>
2017-01-21 16:32:03 +0530

    Removed Sysouts

--- src/main/java/in/javahome/myweb/controller/HelloWorld.java
+++ src/main/java/in/javahome/myweb/controller/HelloWorld.java
@@ -6,13 +6,10 @@ public class HelloWorld {
 		// define limit
 		int limit = 50;
 
-		System.out.println("Printing Even numbers between 1 and " + limit);
-
 		for (int i = 1; i <= limit; i++) {
 
 			// if the number is divisible by 2 then it is even
 			if (i % 2 == 0) {
-				System.out.print(i + " ");
 			}
 		}
 	}
