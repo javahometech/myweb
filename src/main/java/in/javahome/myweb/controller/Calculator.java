a225a2ab32c7318595cc97ef7c08073f2195bb20
Hari <hari@javahome.in>
2017-01-21 18:04:54 +0530

    demo

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -1,6 +1,14 @@
 package in.javahome.myweb.controller;
 
+/*
+ * 
+ */
 public class Calculator {
+	/*
+	 * @param i
+	 * @param j
+	 * @return int
+	 */
 	public int add(int i, int j){
 		return i+j;
 	}
