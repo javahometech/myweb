3693f280c8520c532cb6225310d4a54258b154aa
Hari <hari@javahome.in>
2017-01-21 16:52:43 +0530

    Adding junit test cases

---
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -0,0 +1,11 @@
+package in.javahome.myweb.controller;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+	Calculator cal = new Calculator();
+	public void testAdd(){
+		Assert.assertEquals(cal.add(10, 20), 30);
+	}
+}
