34da936cb8cdc2006e1ab993e39f1c3b9c30544c
javahometech <kammana.hari@gmail.com>
2018-02-27 08:29:14 +0530

    Update sftp_tomcat.sh

--- sftp_tomcat.sh
+++ sftp_tomcat.sh
@@ -1,9 +1,10 @@
 mv target/myweb*.war target/myweb.war
 
 sftp ec2-user@172.31.31.47 <<EOF
-
-  rename /opt/tomcat8/webapps/myweb.war /tmp/myweb.war.back
-  rm /opt/tomcat8/webapps/myweb*
-  put target/myweb.war  /opt/tomcat8/webapps/ 
+  rename /home/ec2-user/tomcat8/webapps/myweb.war /tmp/myweb.war.back
+  rm /home/ec2-user/tomcat8/webapps/myweb*
+  put target/myweb.war  /home/ec2-user/tomcat8/webapps/
   bye
 EOF
+
+ssh ec2-user@172.31.31.47 /home/ec2-user/tomcat8/bin/startup.sh
