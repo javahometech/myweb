9408ef00357d475d78a0a37fbd727de81e1760ca
javahometech <kammana.hari@gmail.com>
2018-04-02 10:00:17 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -9,10 +9,6 @@ public class Calculator {
 	 * @return int
 	 */
 	public int add(int i, int j){
-		String s = null;
-		s.toString();
-		int a = 10;
-		int b = i/0;
 		return i+j;
 	}
 	public int multiply(int i, int j){
